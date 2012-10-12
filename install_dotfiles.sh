7a3823ba58790bff52316787577e193d9bfe17dd
dsk <d.dubandtribal@gmail.com>
2012-10-12 09:33:25 +0900

    Update install script in order to take care of existing .vim directory.

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -2,10 +2,14 @@
 
 cd $(dirname $0)
 
+if [ ! -L $HOME/.vim ] && [ -d $HOME/.vim ]; then
+    echo "Back up .vim directory."
+    mv $HOME/.vim{,.BAK}
+fi
+
 for dotfile in \.?*
 do
-    if [ $dotfile != '..' ] && [ $dotfile != '.git' ] && [ $dotfile != "`echo $dotfile | grep \.swp`" ]
-    then
+    if [ $dotfile != '..' ] && [ $dotfile != '.git' ] && [ $dotfile != "`echo $dotfile | grep \.swp`" ]; then
         ln -Fis "$PWD/$dotfile" $HOME
     fi
 done
