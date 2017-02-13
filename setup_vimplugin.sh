b91469ebebe7f1802caadeabdffcb4f7746f8427
dsk0701 <d.dubandtribal@gmail.com>
2017-02-13 16:53:03 +0900

    Update setup_vimplugin.sh.

--- setup_vimplugin.sh
+++ setup_vimplugin.sh
@@ -29,7 +29,7 @@ fi
 DEIN_DIR=~/.vim/dein
 if [ ! -e "${DEIN_DIR}" ]; then
     curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
-    sh ./installer.sh ~/.vim/dein
+    sh ./installer.sh ${DEIN_DIR}
     rm -f ./installer.sh
 fi
 
