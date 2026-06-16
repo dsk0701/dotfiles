2a04effe67976ace813c97a31d3c47c0868329a8
dsk0701 <d.dubandtribal@gmail.com>
2026-06-16 18:10:15 +0900

    Add git-messenger.vim

--- .config/nvim/lua/init.lua
+++ .config/nvim/lua/init.lua
@@ -22,3 +22,19 @@ vim.keymap.set('n', 'g*', [[g*<Cmd>lua require('hlslens').start()<CR>]], kopts)
 vim.keymap.set('n', 'g#', [[g#<Cmd>lua require('hlslens').start()<CR>]], kopts)
 -- ハイライトとレンズをまとめて消す.
 vim.keymap.set('n', '<Leader>l', '<Cmd>noh<CR>', kopts)
+
+-- カーソル行の commit メッセージをポップアップ表示する.
+-- ポップアップ内 o/O でその行の履歴をさらに遡る/戻る.
+vim.g.git_messenger_no_default_mappings = true
+-- git-messenger はカレントバッファのファイルがあるディレクトリで git を起動する.
+-- fugitive:// など URI スキームのバッファ（リビジョン閲覧）や非ファイルバッファでは
+-- 作業ディレクトリが不正になり E475(expected valid directory) で落ちる.
+-- fugitive のリビジョンバッファは buftype が空なので、実在ファイルかどうかで判定する.
+vim.keymap.set('n', '<Space>gm', function()
+  local file = vim.fn.expand('%:p')
+  if vim.bo.buftype ~= '' or file == '' or vim.fn.filereadable(file) == 0 then
+    vim.notify('git-messenger: 実在するファイルバッファで実行してください', vim.log.levels.WARN)
+    return
+  end
+  vim.cmd('GitMessenger')
+end, { silent = true })
